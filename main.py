399cb8c85808b922fd5a70f369cdcccb8e545322
LorenaManara <lomanara123@gmail.com>
2023-04-08 15:17:19 -0300

    API funcionando

--- main.py
+++ main.py
@@ -10,3 +10,46 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Código inicial do arquivo omitido para facilitação da leitura
+
+@app.post("/api/cursos", response_model=CursoResponse, status_code=status.HTTP_201_CREATED)
+def create(request: CursoRequest, db: Session = Depends(get_db)):
+    curso = CursoRepository.save(db, Curso(**request.dict()))
+    return CursoResponse.from_orm(curso)
+
+
+# Código inicial do arquivo omitido para facilitação da leitura
+
+@app.get("/api/cursos", response_model=list[CursoResponse])
+def find_all(db: Session = Depends(get_db)):
+    cursos = CursoRepository.find_all(db)
+    return [CursoResponse.from_orm(curso) for curso in cursos]
+
+# Código inicial do arquivo omitido para facilitação da leitura
+
+@app.get("/api/cursos/{id}", response_model=CursoResponse)
+def find_by_id(id: int, db: Session = Depends(get_db)):
+    curso = CursoRepository.find_by_id(db, id)
+    if not curso:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado"
+        )
+    return CursoResponse.from_orm(curso)
+
+@app.delete("/api/cursos/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_by_id(id: int, db: Session = Depends(get_db)):
+    if not CursoRepository.exists_by_id(db, id):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado"
+        )
+    CursoRepository.delete_by_id(db, id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/api/cursos/{id}", response_model=CursoResponse)
+def update(id: int, request: CursoRequest, db: Session = Depends(get_db)):
+    if not CursoRepository.exists_by_id(db, id):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado"
+        )
+    curso = CursoRepository.save(db, Curso(id=id, **request.dict()))
+    return CursoResponse.from_orm(curso)
